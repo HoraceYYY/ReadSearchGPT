f14087e7e947e1348132950861f5c7c8d86b6e53
Horace Yu <horace@Horaces-MacBook-Pro.local>
2023-05-06 16:01:09 -0700

    add main file, not working yet

--- ResearchGPT/Google/async_google.py
+++ ResearchGPT/Google/async_google.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import async_utils
 
 ## take the google search query and return the top 8 results URL
-def google_official_search(query: str, num_results: int = 10) -> str | list[str]:
+def google_official_search(query: str, num_results: int = 8) -> str | list[str]:
     """Return the results of a Google search using the official Google API
 
     Args:
--- ResearchGPT/researchGPT.py
+++ ResearchGPT/researchGPT.py
@@ -10,8 +10,10 @@ if __name__ == "__main__":
     topic = input(colored("What would you like to search:", "blue", attrs=["bold", "underline"]) + " ")
     print(colored("\nPlease list 3 outcomes your would like to achieve!", "blue",attrs=["bold", "underline"]))
     objectives_input = [input(colored(f"Objective {i + 1}: ", "blue", attrs=["bold"])) for i in range(3)]
+    print(objectives_input)
     non_empty_objectives = [f"{i + 1}. {obj}" for i, obj in enumerate(objectives_input) if obj]
     objectives = topic + "\n"+ "\n".join(non_empty_objectives)
+    print(objectives)
     print(colored("\nIf there is a domain you would like to search within, paste any link from the domain. Otherwise enter 'None'.", "blue", attrs=["bold", "underline"]))
     searchDomain = input(colored("Search Domain: ", "blue", attrs=["bold"])).lower()
     max_depth = utils.searchType()
