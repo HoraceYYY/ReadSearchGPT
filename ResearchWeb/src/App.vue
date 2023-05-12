1ce3d6f021927087fe832740d05e754dd0db8cb6
Horace Yu <horace@Horaces-MacBook-Pro.local>
2023-05-11 21:33:30 -0700

    frontend started

--- ResearchWeb/src/App.vue
+++ ResearchWeb/src/App.vue
@@ -1,85 +1,90 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <nav class="navbar">
+      <div class="navbar-top">
+        <div class="navbar-left">
+          <router-link to="/" class="nav-link">New Search</router-link>
+          <router-link to="/mysearch" class="nav-link">Find My Search</router-link>
+        </div>
+        <div class="navbar-right">
+          <router-link to="/tutorial" class="nav-link">Watch Tutorial</router-link>
+          <router-link to="/feedback" class="nav-link">Provide Feedback</router-link>
+        </div>
+      </div>
+    </nav>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="content">
+      <h1> (Re)Search For Me </h1>
+      <router-view />
     </div>
-  </header>
-
-  <RouterView />
+    
+    <nav class="navbar">  
+      <div class="navbar-bottom-right">
+        <router-link to="/privacy" class="nav-link">Privacy</router-link>
+        <router-link to="/terms" class="nav-link">Terms</router-link>
+      </div>
+    </nav>
+  </div>
 </template>
 
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+html, body {
 
-nav {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 100%;
+}
+h1 {
+  font-size: 3em; /* Adjust this value to change the size of the text */
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  justify-content: space-between;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.navbar {
+  background-color: #f8f9fa;
+  padding: 10px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.navbar-top {
+  display: flex;
+  justify-content: space-between;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.navbar-left, .navbar-right, .navbar-bottom-right {
+  display: flex;
+  gap: 20px;
 }
 
-nav a:first-of-type {
-  border: 0;
+.navbar-bottom-right {
+  justify-content: flex-end;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.nav-link {
+  text-decoration: none;
+  color: #333;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  flex-grow: 1;
+  gap: 20px;
+  padding-top: 10vh; /* Adjust this value to move content up or down */
 }
 </style>
--- ResearchWeb/src/router/index.js
+++ ResearchWeb/src/router/index.js
@@ -1,22 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import NewSearch from '../views/NewSearch.vue'
+import MySearch from '../views/MySearch.vue'
+import Tutorial from '../views/Tutorial.vue'
+import Feedback from '../views/Feedback.vue'
+import Terms from '../views/Terms.vue'
+import Privacy from '../views/Privacy.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'NewSearch',
+      component: NewSearch
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/mysearch',
+      name: 'Find My Search',
+      component: MySearch
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      //component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/tutorial', 
+      name: 'Find My Tutorial',
+      component: Tutorial 
+    },
+    {
+      path: '/feedback', 
+      name: 'Provide Feedback',
+      component: Feedback 
+    },
+    {
+      path: '/terms', 
+      name: 'Terms',
+      component: Terms 
+    },
+    {
+      path: '/privacy', 
+      name: 'Privacy',
+      component: Privacy 
+    },
   ]
 })
 
