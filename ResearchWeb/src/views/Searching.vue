42e68d2d880b20ab27fcf7ba5526b342ee5443a3
Horace Yu <horace@Horaces-MacBook-Pro.local>
2023-05-14 21:10:00 -0700

    result page is done. not tested

--- ResearchWeb/src/views/Searching.vue
+++ ResearchWeb/src/views/Searching.vue
@@ -17,64 +17,103 @@ export default {
 </script>
 
 <template>
-  <div class="results-container">
-    <p>Search Results:</p>
-    <pre>{{ jsonData }}</pre>
-    <div class="button-container">
-      <button type="button" @click="newSearch" class="startover-button">New Search</button>
+    <div class="results-container">
+      <div class="text-container">
+        <p>Your AI agent has begun your research!</p>
+        <p>Download partial results anytime during the search. Full results remain available for another 24 hours and will be automatically deleted.</p>
+        <p>You need the Task ID to retrieve the results. <span class="important-notice">Make sure to save the Task ID. It won't be displayed again.</span></p>
+
+      </div>
+      <table class="results-table">
+        <tr v-for="(value, key) in jsonData" :key="key">
+          <td class="key-column">{{ key }}</td>
+          <td :class="{'task-id-value': key === 'task_id'}">{{ value }}</td>
+        </tr>
+      </table>
+      <div class="button-container">
+        <button type="button" @click="newSearch" class="startover-button">New Search</button>
+      </div>
     </div>
-  </div>
-</template>
-
-<style scoped>
-.results-container {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.results-container p {
-  font-family: Arial, sans-serif;
-  font-size: 20px;
-  margin-left: -100px;
-  font-weight: bold;
-  width: 800px;
-  margin-bottom: 40px;
-}
-
-pre {
-  width: 900px;
-  font-family: Arial, sans-serif;
-  font-size: 20px;
-  margin-bottom: 20px;
-  overflow: auto;
-}
-
-.button-container {
-  display: flex;
-  justify-content: center;
-  gap: 120px;
-  margin-top: 80px;
-}
-
-.startover-button {
-  border: 2px solid #0c952c;
-  border-radius: 8px;
-  background-color: #ffffff;
-  color: #0c952c;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-
-.startover-button:hover {
-  background-color: #0c952c;
-  color: #ffffff;
-}
-</style>
+  </template>
+  
+  <style scoped>
+  .results-container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .text-container {
+    max-width: 1000px; /* Match the table width */
+    width: 100%;
+    text-align: left;
+  }
+  
+  .results-container p {
+    font-family: Arial, sans-serif;
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+  
+  .important-notice {
+    font-weight: bold;
+    color: red;
+  }
+  
+  .search-results-title {
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+  
+  .results-table {
+    width: 900px;
+    font-family: Arial, sans-serif;
+    font-size: 20px;
+    margin-bottom: 20px;
+    border-collapse: collapse;
+    margin-top: 20px;
+  }
+  
+  .results-table td {
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+  
+  .key-column {
+    width: 200px; /* Adjust as needed */
+  }
+  
+  .task-id-value {
+    font-weight: bold;
+    color: red;
+  }
+  
+  .button-container {
+    display: flex;
+    justify-content: center;
+    gap: 120px;
+    margin-top: 80px;
+  }
+  
+  .startover-button {
+    border: 2px solid #0c952c;
+    border-radius: 8px;
+    background-color: #ffffff;
+    color: #0c952c;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+  
+  .startover-button:hover {
+    background-color: #0c952c;
+    color: #ffffff;
+  }
+  </style>
+  
