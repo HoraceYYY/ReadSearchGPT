6241769d340f8bd9bb12ba0221cacb50570b1f0d
Horace Yu <horace@Horaces-MacBook-Pro.local>
2023-05-18 17:32:22 -0700

    stop api with database working now

--- ResearchGPT/Google/async_google.py
+++ ResearchGPT/Google/async_google.py
@@ -69,64 +69,69 @@ def google_official_search(query: str, searchWidth: int) -> str | list[str]:
 
 async def url_consumer(task_id, consumer_queue, consumer_checked_list, content_prompt, results, producer_done, api_key):
     while not producer_done[0] or not consumer_queue.empty():
-        url, depth = await consumer_queue.get()
-        wrapped_url = async_utils.Url(url)
-        if wrapped_url not in consumer_checked_list:
-            consumer_checked_list.add(wrapped_url)
-            soup, content_type, status_code = await async_utils.fetch_url(url, task_id, results) # fetch the url
-            if status_code == 200:
-                if content_type.lower() == 'application/pdf': # check if the content is pdf and download it
-                    results['Unchecked Material'] = pd.concat([results['PDFs'], pd.DataFrame([{'PDFs': url}])], ignore_index=True)
-                    await async_utils.updateExcel(task_id, "results", "Unchecked Material", results['Unchecked Material'])
-                    #await async_utils.download_pdf(url)
-                    continue
-                print(colored('\n\U0001F9D0 Consumer: Reading the website for queried information: ', 'yellow', attrs=['bold']), url)
-                content, page_Title = async_utils.getWebpageData(soup) # get the page title,content, and links
-                pageSummary = await async_utils.PageResult(api_key, content_prompt, content) # get the page summary based on the search query
-                
-                if "4b76bd04151ea7384625746cecdb8ab293f261d4" not in pageSummary.lower():
-                    results['Related'] = pd.concat([results['Related'], pd.DataFrame([{'URL': url, 'Title': page_Title, 'Content': pageSummary}])], ignore_index=True) # add the filtered result to the dataframe
-                    await async_utils.updateExcel(task_id, "results", "Related", results['Related'])                
-                else:
-                    results['Unrelated'] = pd.concat([results['Unrelated'], pd.DataFrame([{'URL': url, 'Title': page_Title, 'Content': pageSummary}])], ignore_index=True)
-                    await async_utils.updateExcel(task_id, "results", "Unrelated", results['Unrelated'])
+        try:
+            url, depth = await consumer_queue.get()
+            wrapped_url = async_utils.Url(url)
+            if wrapped_url not in consumer_checked_list:
+                consumer_checked_list.add(wrapped_url)
+                soup, content_type, status_code = await async_utils.fetch_url(url, task_id, results) # fetch the url
+                if status_code == 200:
+                    if content_type.lower() == 'application/pdf': # check if the content is pdf and download it
+                        results['Unchecked Material'] = pd.concat([results['PDFs'], pd.DataFrame([{'PDFs': url}])], ignore_index=True)
+                        await async_utils.updateExcel(task_id, "results", "Unchecked Material", results['Unchecked Material'])
+                        #await async_utils.download_pdf(url)
+                        continue
+                    print(colored('\n\U0001F9D0 Consumer: Reading the website for queried information: ', 'yellow', attrs=['bold']), url)
+                    content, page_Title = async_utils.getWebpageData(soup) # get the page title,content, and links
+                    pageSummary = await async_utils.PageResult(api_key, content_prompt, content) # get the page summary based on the search query
+                    
+                    if "4b76bd04151ea7384625746cecdb8ab293f261d4" not in pageSummary.lower():
+                        results['Related'] = pd.concat([results['Related'], pd.DataFrame([{'URL': url, 'Title': page_Title, 'Content': pageSummary}])], ignore_index=True) # add the filtered result to the dataframe
+                        await async_utils.updateExcel(task_id, "results", "Related", results['Related'])                
+                    else:
+                        results['Unrelated'] = pd.concat([results['Unrelated'], pd.DataFrame([{'URL': url, 'Title': page_Title, 'Content': pageSummary}])], ignore_index=True)
+                        await async_utils.updateExcel(task_id, "results", "Unrelated", results['Unrelated'])
 
-                print("\u2714\uFE0F", colored(' Consumer: Done! Results has been saved!','green',attrs=['bold']), ' Current Depth: ', depth)
+                    print("\u2714\uFE0F", colored(' Consumer: Done! Results has been saved!','green',attrs=['bold']), ' Current Depth: ', depth)
+                else:
+                    print("\U0001F6AB", colored(f' Consumer: Website did not respond. Error code: {status_code}.','red',attrs=['bold']), ' Current Depth: ', depth, ' URL:', url)
             else:
-                print("\U0001F6AB", colored(f' Consumer: Website did not respond. Error code: {status_code}.','red',attrs=['bold']), ' Current Depth: ', depth, ' URL:', url)
-        else:
-            print(colored('\u2714\uFE0F  Consumer:The content in this URL has already been checked:', 'green', attrs=['bold']), f' {url}')
-            print(colored('\u2714\uFE0F  Consumer: Skip to the next website.\n', 'green', attrs=['bold']))
+                print(colored('\u2714\uFE0F  Consumer:The content in this URL has already been checked:', 'green', attrs=['bold']), f' {url}')
+                print(colored('\u2714\uFE0F  Consumer: Skip to the next website.\n', 'green', attrs=['bold']))
+        except asyncio.CancelledError:
+            print(colored('\u2714\uFE0F  Consumer: Task Cancelled!','red',attrs=['bold']))
     print(colored('\u2714\uFE0F  Consumer: Done!','green',attrs=['bold']))
 
 async def url_producer(producer_queue, consumer_queue, producer_checked_list, searchDomain, url_prompt, max_depth, producer_done, api_key):
     while not producer_queue.empty():
-        url, depth = await producer_queue.get()
-
-        if depth < max_depth: ## change this to max_depth back later 
-            wrapped_url = async_utils.Url(url)
-            if wrapped_url not in producer_checked_list:
-                producer_checked_list.add(wrapped_url)
-                print(colored(f'\U0001F9D0 Producer: Seaching for additonal relavent websites on {url}', 'yellow', attrs=['bold']))
-                soup, content_type, status_code = await async_utils.fetch_url(url) # fetch the url
-                if status_code == 200:
-                    if content_type.lower() == 'application/pdf': # check if the content is pdf, if so, skip to the next url
-                        continue
-                    links = async_utils.getWebpageLinks(soup, searchDomain, url)
-                    #print(links)
-                    relaventURLs = await async_utils.relaventURL(url_prompt, links, api_key) # Get the highly relevant links from the page and make them into asbolute URLs
-                    if relaventURLs:  
-                        print("\u2714\uFE0F", colored(' Producer: Additional relavent websites to search:', 'green', attrs=['bold']) ,f" {relaventURLs}", '\n')  
-                        for new_url in relaventURLs:
-                            await producer_queue.put((new_url, depth + 1))
-                            await consumer_queue.put((new_url, depth + 1))
+        try:
+            url, depth = await producer_queue.get()
+            if depth < max_depth: ## change this to max_depth back later 
+                wrapped_url = async_utils.Url(url)
+                if wrapped_url not in producer_checked_list:
+                    producer_checked_list.add(wrapped_url)
+                    print(colored(f'\U0001F9D0 Producer: Seaching for additonal relavent websites on {url}', 'yellow', attrs=['bold']))
+                    soup, content_type, status_code = await async_utils.fetch_url(url) # fetch the url
+                    if status_code == 200:
+                        if content_type.lower() == 'application/pdf': # check if the content is pdf, if so, skip to the next url
+                            continue
+                        links = async_utils.getWebpageLinks(soup, searchDomain, url)
+                        #print(links)
+                        relaventURLs = await async_utils.relaventURL(url_prompt, links, api_key) # Get the highly relevant links from the page and make them into asbolute URLs
+                        if relaventURLs:  
+                            print("\u2714\uFE0F", colored(' Producer: Additional relavent websites to search:', 'green', attrs=['bold']) ,f" {relaventURLs}", '\n')  
+                            for new_url in relaventURLs:
+                                await producer_queue.put((new_url, depth + 1))
+                                await consumer_queue.put((new_url, depth + 1))
+                        else:
+                            print("\u2714\uFE0F", colored(f' Producer: No additional relavent webisites found on {url}.\n', 'green', attrs=['bold']))
                     else:
-                        print("\u2714\uFE0F", colored(f' Producer: No additional relavent webisites found on {url}.\n', 'green', attrs=['bold']))
+                        print("\U0001F6AB", colored(f' Producer: Website did not respond. Error code: {status_code}.','red',attrs=['bold']), ' Current Depth: ', depth, ' URL:', url , '\n')
                 else:
-                    print("\U0001F6AB", colored(f' Producer: Website did not respond. Error code: {status_code}.','red',attrs=['bold']), ' Current Depth: ', depth, ' URL:', url , '\n')
-            else:
-                print(colored('\u2714\uFE0F Producer: URLs on this page have already been checked:', 'green', attrs=['bold']), f' {url}')
-                print(colored('\u2714\uFE0F  Producer: Skip to the next website.\n', 'green', attrs=['bold']))
+                    print(colored('\u2714\uFE0F Producer: URLs on this page have already been checked:', 'green', attrs=['bold']), f' {url}')
+                    print(colored('\u2714\uFE0F  Producer: Skip to the next website.\n', 'green', attrs=['bold']))
+        except asyncio.CancelledError:
+            print(colored('\u2714\uFE0F  Producer: Task Cancelled!','red',attrs=['bold']))
     producer_done[0] = True  # Signal the consumer that the producer is done
     print(colored('\u2714\uFE0F  Producer: Done!','green',attrs=['bold']))
 
