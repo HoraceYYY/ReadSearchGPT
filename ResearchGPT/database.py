e0cec1b1877306f69169b7e35de945d1abac698d
Horace Yu <horace@Horaces-MacBook-Pro.local>
2023-05-21 20:10:35 -0700

    gcp database connected

--- ResearchGPT/database.py
+++ ResearchGPT/database.py
@@ -3,7 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://readsearch:readsearch@localhost:5432/tasks"
+"""
+local database vs GCP database. Only choose one
+"""
+##SQLALCHEMY_DATABASE_URL = "postgresql://readsearch:readsearch@localhost:5432/tasks"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:readsearchpostgres@34.66.209.78:5432/tasks"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
