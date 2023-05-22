6d656b6cd2ac2d50bd9f6d9318ff920f52bf2b5a
Horace Yu <horace@Horaces-MacBook-Pro.local>
2023-05-22 12:07:08 -0700

    minor code change

--- ResearchGPT/database.py
+++ ResearchGPT/database.py
@@ -7,7 +7,6 @@ import os
 dotenv_path = os.path.join(os.path.dirname(__file__), 'Google', '.env')
 load_dotenv(dotenv_path)  # Load the .env file
 SQLALCHEMY_DATABASE_URL = os.getenv("GCP_SQLALCHEMY_DATABASE_URL")
-print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
