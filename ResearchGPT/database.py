d512144cf7cc45947cc0ac61ea89f643d529fbcf
Horace Yu <horace@Horaces-MacBook-Pro.local>
2023-05-22 19:15:32 -0700

    update deployment database connection

--- ResearchGPT/database.py
+++ ResearchGPT/database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
-from dotenv import load_dotenv
-import os
+# from dotenv import load_dotenv
+# import os
 
-dotenv_path = os.path.join(os.path.dirname(__file__), 'Google', '.env')
-load_dotenv(dotenv_path)  # Load the .env file
-SQLALCHEMY_DATABASE_URL = os.getenv("GCP_SQLALCHEMY_DATABASE_URL")
+# dotenv_path = os.path.join(os.path.dirname(__file__), 'Google', '.env')
+# load_dotenv(dotenv_path)  # Load the .env file
+# SQLALCHEMY_DATABASE_URL = os.getenv("GCP_SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql+pg8000://postgres:readsearchpostgres@/tasks?unix_sock=/cloudsql/readsearch:us-central1:readsearch-db/.s.PGSQL.5432"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
