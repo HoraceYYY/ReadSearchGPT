2ac178b8667d3a2da8604d921967b087cf5a17f0
Horace Yu <horace@Horaces-MacBook-Pro.local>
2023-05-22 23:53:09 -0700

    fix postgresql link

--- ResearchGPT/database.py
+++ ResearchGPT/database.py
@@ -7,7 +7,7 @@ from models import Base
 # dotenv_path = os.path.join(os.path.dirname(__file__), 'Google', '.env')
 # load_dotenv(dotenv_path)  # Load the .env file
 # SQLALCHEMY_DATABASE_URL = os.getenv("GCP_SQLALCHEMY_DATABASE_URL")
-SQLALCHEMY_DATABASE_URL = "postgresql+pg8000://postgres:readsearchpostgres@/tasks?unix_sock=/cloudsql/readsearch:us-central1:readsearch/.s.PGSQL.5432"
+SQLALCHEMY_DATABASE_URL = "postgresql+pg8000://postgres:readsearchpostgres@/tasks?unix_sock=/cloudsql/readsearch:us-central1:readsearchubc/.s.PGSQL.5432"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
