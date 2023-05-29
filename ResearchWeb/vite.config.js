c07a13275e7bb93974c580bb90626937661e84b4
Horace Yu <horace@Horaces-MacBook-Pro.local>
2023-05-28 21:38:17 -0700

    add cache busting code to make sure each update will buste the browser cache

--- ResearchWeb/vite.config.js
+++ ResearchWeb/vite.config.js
@@ -7,6 +7,18 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    manifest: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/[name]-[hash].js',
+        chunkFileNames: 'js/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
